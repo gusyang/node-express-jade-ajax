c770aa6c66b1d845e6c1d4474bf8af4cf36d47d3
gus <gus.usa@aol.com>
2015-02-13 23:22:58 -0800

    first commit

---
+++ service/routes/users.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/userlist',function(req,res){
+    var db = req.db;
+    db.collection('userlist').find().toArray(function(err,items){
+        res.json(items);
+    });
+});
+
+router.post('/adduser', function (req,res) {
+    var db = req.db;
+    db.collection('userlist').insert(req.body,function(err,result){
+        res.send(err === null?{msg:''}:{msg:err});
+    });
+});
+
+router.delete('/deleteuser/:id',function(req,res){
+    var db = req.db;
+    var id = req.params.id;
+    db.collection('userlist').removeById(id,function(err,result){
+       res.send(result === 1?{msg:''}:{msg:'err:'+err});
+    });
+});
+
+
+module.exports = router;
